7805512061c2d8f86d6fa86076519b64c5ef5803
adeelkbw <adeelnasirkbw@gmail.com>
2022-04-26 07:22:00 +0500

    all setup done starting building the endpoint --server

---
+++ backend/routes/product.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = require('../app');
+const router = express.Router();
+
+const { } = require('../controllers/product')
+
+
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,30 @@
 const app = require('./app');
 const colors = require('colors');
 const dotenv = require('dotenv');
-
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const routes = fs.readdirSync('./routes')
+const connectDB = require('./config/db')
 dotenv.config({ path: './config/config.env' })
 
 
+connectDB()
 const PORT = process.env.PORT || 5000;
+app.use(cors());
+if (process.env.NODE_ENV = "development") {
+    app.use(morgan("dev"))
+}
 
-const server = app.listen(PORT, console.log(`Server is running up on port ${PORT}`.cyan))
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+//routes
+routes.map((r) => app.use('/api/v1', require('./routes/' + r)))
+
+
+
+const server = app.listen(PORT, console.log(`Backend is listening ${process.env.NODE_ENV} at port ${PORT}`.brightCyan.bold))
 
 process.on('unhandledRejection', (err, promise) => {
     console.log(`ERROR ===> ${err.message}`);
