65c3e4c61fa2a1fa359669fb6d1e56d28c5b9d06
adeelkbw <adeelnasirkbw@gmail.com>
2022-04-26 07:40:12 +0500

    Products Schema defined --server

---
+++ backend/models/product.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: [true, "Name Is required"],
+        maxlength: [40, "Name Should have max 20 chars"],
+        trim: true
+    },
+    description: {
+        type: String,
+        required: [true, "description Is required"],
+        maxlength: [400, "Name Should have max 20 chars"]
+    },
+    price: {
+        type: Number,
+        required: [true, "price Is required"],
+        maxlength: [8, "Name Should have max 20 chars"]
+    },
+    ratings: {
+        type: Number,
+        required: [true, "price Is required"],
+        default: 0
+    },
+    images: {
+        type: Array
+    },
+    category: {
+        type: String,
+        required: [true, "Enter the product category"],
+        trim: true
+    },
+    stock: {
+        type: Number,
+        required: [true, "Enter the product category"],
+        maxlength: [4, "stock Should have max 4 chars"],
+        default: 1
+
+    },
+    numberOfReviews: {
+        type: Number,
+        default: 0
+    },
+    reviews: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            ratings: {
+                type: Number,
+                required: true
+            },
+            comment: {
+                type: String,
+                required: true
+            }
+        }
+    ]
+
+
+}, { timestamps: true })
+
+module.exports = mongoose.model("Product", productSchema)
